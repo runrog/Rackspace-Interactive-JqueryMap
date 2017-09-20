460517bfbf0dbb0d5a91edd8b53b5bbe08f70248
roge5716 <roger.avalos@rackspace.com>
2017-09-20 11:42:12 -0500

    adding all new files

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-## Node + EJS boilderplate
+## Rackspace Interactive SVG Map
+This is a WIP project that creates a new svg map with custom bubbles/tooltips created from a config using jQuery.
 
-### This boilerplate includes the following
-* Gulp (automation)
-* EJS (templating)
-* BrowserSync (live reloading)
-* SASS
-* ES6 (gulp-babel)
-* eslint (js linting)
+![alt text](rs-map-demo.png "")
+
+[See Demo](https://0fb555c65f288d09a992-a2085a232a1d2a458a641f48ff12bf08.ssl.cf2.rackcdn.com/rs-interactive-map/index.html)
 
 ### Install
 ```
@@ -15,8 +12,54 @@ npm install
 ```
 npm start
 ```
+You're browser should automatically launch at localhost 2004.
+
+### API
+To initiate the map you need to include the js and css then pass a config of bubbles and the map selector into the init function. Read more on the config further down.
+```
+<link rel="stylesheet" type="text/css" href="css/main.min.css">
+<script src="js/app.min.js" charset="utf-8"></script>
+
+<div class="rsMap-container"></div>
+
+<script type="text/javascript">
+  const config = {
+    circles: [
+      {
+        type: 'dataCenter',
+        items: 2,
+        tipTitle: 'This is a Title',
+        tipBody: 'This is some Text',
+        tipFooter: 'This is footer text',
+        top: '130px',
+        left: '130px',
+      },
+      // ... enter as many as you like
+    ]
+  };
+  rsInteractiveMap.init(config, $('.rsMap-container'));
+</script>
+```
+
+## Config
+The following are options available:
+```
+type: // String value. The options for this are 'dataCenter', 'office' and 'hub'
+items: // Number value. This will represent the number inside the bubble.
+tipTitle: // String value. This is the title in the tip, it can accept HTML.
+tipBody: // String value. This is the body text in the tip, it can accept HTML.
+tipFooter: // String value. This is the footer text in the tip, it can accept HTML.
+top: // String value. This is the px value from the top of the map you would like it.
+left: // String value. This is the px value from the left of the map you would like it.
+```
+
 ### Gulp Tasks
 Gulp tasks will run automatically but to manually run them:
+
+Build for dist
+```
+gulp build-dist
+```
 
 Building Javascript: This will concat the js files together in order by name:
 ```
---
+++ js/rs-map.js
@@ -0,0 +1,82 @@
+/*
+ The Rackspace Interactive Map plugin can be built with an array of map circles.
+ See below for parameter options passed through the config.
+ * @param {string} type - Current options for this property are 'dataCenter', 'office', 'hub'.
+ * @param {number} items - Th
+ * @param {string} tipTitle - accepts text and HTML
+ * @param {string} tipBody - accepts text and HTML
+ * @param {string} tipFooter - accepts text and HTML
+ * @param {string} top - px value of top position in the map
+ * @param {string} left - px value of left position in the map.
+*/
+
+const rsInteractiveMap = {
+  mapSelector: null,
+  buildCircles(circles) {
+    for (let i = 0; i < circles.length; i += 1) {
+      const circle = circles[i];
+      const $template = $(`
+      <div class="rsMap-${circle.type}" style="top:${circle.top};left:${circle.left};">
+        <div class="rsMap-inner">
+          <div class="rsMap-toolTip">
+            <span class="rsMap-toolTip-header">${circle.tipTitle}</span>
+            <p class="rsMap-toolTip-body">${circle.tipBody}</p>
+            <span class="rsMap-toolTip-footer">${circle.tipFooter}</span>
+            <div class="rsMap-toolTip-arrow"></div>
+          </div>
+          <div class="rsMap-${circle.type}-circle">${circle.items}</div>
+        </div>
+      </div>`);
+      this.mapSelector.append($template);
+      this.positionTips($template, circle.type);
+      // attach event listeners
+      this.hoverCircle($template);
+    }
+  },
+  positionTips($el, type) {
+    const $tipContainer = $el.find('.rsMap-toolTip');
+    const $circle = $el.find(`.rsMap-${type}-circle`);
+    const $tipHeight = $tipContainer.outerHeight();
+    const $tipWidth = $tipContainer.outerWidth();
+    const $circleWidth = $circle.outerWidth();
+    // we need to make sure that the tip box is always
+    // centered above bubble even if dimensions change
+    $tipContainer.css({
+      top: -($tipHeight + 15),
+      left: -(($tipWidth / 2) - ($circleWidth / 2)),
+    });
+  },
+  hoverTimeout: null,
+  hoverCircle($el) {
+    const $tipContainer = $el.find('.rsMap-toolTip');
+    $el.hover(() => {
+      clearTimeout(this.hoverTimeout);
+      // hide any open bubbles on the page except active one, then show hovered tip
+      // this will check to make sure the active is not hiding. This will avoid a flicker on hover
+      $('.rsMap-toolTip').each(function hideAll() {
+        const $this = $(this);
+        const $wrapper = $this.parent().parent();
+        const $index = $wrapper.index();
+        const $elIndex = $el.index();
+        if ($index !== $elIndex) {
+          $this.hide();
+          $wrapper.css('z-index', '');
+        }
+      });
+      $el.css('z-index', 5);
+      $tipContainer.fadeIn(200, () => {
+        $el.css('z-index', 5);
+      });
+    }, () => {
+      this.hoverTimeout = setTimeout(() => {
+        $tipContainer.fadeOut(200, () => {
+          $('.rsMap-inner').parent().css('z-index', '');
+        });
+      }, 500);
+    });
+  },
+  init(config, map) {
+    this.mapSelector = map;
+    this.buildCircles(config.circles);
+  },
+};
